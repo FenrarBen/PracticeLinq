b43dbde51339fca747c131f44dd57a268336eef5
BenStrawbridge <lute_midnight@yahoo.com>
2020-11-11 10:51:58 -0600

    Linq Exercise 1

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>();
+            videoGames.Add("Final Fantasy VII");
+            videoGames.Add("Castlevania: Symphony of the Night");
+            videoGames.Add("World of Warcraft");
+            videoGames.Add("StarCraft");
+            videoGames.Add("Borderlands 2");
+            videoGames.Add("Chrono Cross");
+            videoGames.Add("Vampyr");
+            videoGames.Add("Cyberpunk 2077");
+            videoGames.Add("Super Mario Bros. 3");
+
+            var byLength = videoGames.OrderBy(x => x);
+            foreach (var game in byLength)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
